115507622e46353b4aa5c924621a2426f917b711
richaagarwal123 <165362283+richaagarwal123@users.noreply.github.com>
2026-02-10 18:48:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let hmenu=document.getElementById("con-menu")
+
+
+function toggleMenu(){
+    if(hmenu.style.display=="flex")
+    {
+       hmenu.style.display="none"
+    }
+    else{
+        hmenu.style.display="flex"
+    }
+}
